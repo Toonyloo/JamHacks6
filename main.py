f87a2c71194e6e896dd100e2516abf13847a4677
Tony Liu <tony0818abc@gmail.com>
2022-05-21 12:19:11 -0400

    Completed title screen functionality

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ pygame.display.set_caption('Game')  # TODO
 clock = pygame.time.Clock()  
 
 running = True
+game_state = 0
 
 def draw_title_screen():
     screen.fill((0, 140, 148))
@@ -31,40 +32,55 @@ def draw_title_screen():
     screen.blit(Images.LOGO, (315, 60))
     screen.blit(Images.BACKGROUND, (0, 0))
 
-player = Player()
-arrow = Arrow(player)
-terrains = []
-terrains.append(Ground(0, 700, 1280, 20, "floor"))
-terrains.append(Ground(0, 0, 20, 720, "left wall"))
-terrains.append(Ground(1260, 0, 20, 720, "right wall"))
-terrains.append(Ground(400, 600, 200, 20, "platform"))
-terrains.append(Ground(300, 400, 200, 20, "platform"))
+
 while running:
     clock.tick(60) 
     events = pygame.event.get()
     keys = pygame.key.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
+    mouse_buttons = pygame.mouse.get_pressed()
     
     for event in events:
         if event.type == pygame.QUIT:
             running = False
     screen.fill(WHITE)
     
-    if keys[pygame.K_SPACE] and arrow.attached:
-        arrow.shoot(mouse_pos)
-    player.handle_inputs(keys, events)
-    for ground in terrains:
-        # print(f"{ground.tag}, Bottom: {player.terrain_collision_bottom(ground)}, Top: {player.terrain_collision_top(ground)}, Left: {player.terrain_collision_left(ground)}, Right: {player.terrain_collision_right(ground)}")
-        player.terrain_collision(ground)
-        arrow.terrain_collision(ground)
-        ground.draw(screen)
-    player.handle_movement()
-    arrow.handle_movement()
-    
-    player.draw(screen) 
-    arrow.draw(screen)
-    
-    draw_title_screen()
+    if game_state == 0:
+        draw_title_screen()
+        if 535 < mouse_pos[0] < 745 and 400 < mouse_pos[1] < 500:
+            rect = pygame.Rect(535, 400, 210, 94)
+            pygame.draw.rect(screen, (128, 128, 128), rect, 5)
+            if mouse_buttons[0]:
+                game_state = 1
+                player = Player()
+                arrow = Arrow(player)
+                terrains = []
+                terrains.append(Ground(0, 700, 1280, 20, "floor"))
+                terrains.append(Ground(0, 0, 20, 720, "left wall"))
+                terrains.append(Ground(1260, 0, 20, 720, "right wall"))
+                terrains.append(Ground(400, 600, 200, 20, "platform"))
+                terrains.append(Ground(300, 400, 200, 20, "platform"))
+        if 528 < mouse_pos[0] < 748 and 530 < mouse_pos[1] < 630:
+            rect = pygame.Rect(535, 529, 215, 95)
+            pygame.draw.rect(screen, (128, 128, 128), rect, 5)
+            if mouse_buttons[0]:
+                pygame.quit()
+                break
+    if game_state == 1:
+        if keys[pygame.K_SPACE] and arrow.attached:
+            arrow.shoot(mouse_pos)
+        player.handle_inputs(keys, events)
+        for ground in terrains:
+            # print(f"{ground.tag}, Bottom: {player.terrain_collision_bottom(ground)}, Top: {player.terrain_collision_top(ground)}, Left: {player.terrain_collision_left(ground)}, Right: {player.terrain_collision_right(ground)}")
+            player.terrain_collision(ground)
+            arrow.terrain_collision(ground)
+            ground.draw(screen)
+        player.handle_movement()
+        arrow.handle_movement()
+        
+        player.draw(screen) 
+        arrow.draw(screen)
+        
     pygame.display.flip()
 
 pygame.quit()
